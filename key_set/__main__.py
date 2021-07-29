771b422e2b3650132b6ab642b4a87f18370872b7
Eduardo Turiño <eturino@eturino.com>
2021-07-29 17:29:04 +0200

    chore: remove main stuff

--- key_set/__main__.py
+++ key_set/__main__.py
@@ -4,8 +4,10 @@
 A module's __name__ is set equal to '__main__' when read from standard input,
 a script, or from an interactive prompt.
 """
-
-from key_set.utils import add_two_numbers
+from key_set.base import KeySetAll, KeySetAllExceptSome, KeySetNone, KeySetSome
 
 print('Executed from command line...')
-print(f'2 + 2 equals {add_two_numbers(2, 2)}')
+print(f'all: {KeySetAll()}')
+print(f'none: {KeySetNone()}')
+print(f'some: {KeySetSome(["A", "B"])}')
+print(f'all except some: {KeySetAllExceptSome(["A", "B"])}')
