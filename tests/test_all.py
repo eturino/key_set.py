20d1f8060d4adf88805916eb4c40c4b8222d470d
Eduardo Turiño <eturino@eturino.com>
2021-07-29 11:06:46 +0200

    fix: fixing tests and circular dependencies

---
+++ tests/test_all.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import __future__  # noqa: F401
+
+from key_set.base import KeySetAll
+
+
+class TestAll:  # noqa: D101
+
+    def test_represents(self) -> None:
+        ks = KeySetAll()
+        assert ks.represents_all()
+        assert not ks.represents_none()
+        assert not ks.represents_some()
+        assert not ks.represents_all_except_some()
+
+    def test_invert(self) -> None:
+        ks = KeySetAll()
+        actual = ks.invert()
+        assert actual.represents_none()
+
+    def test_elements(self) -> None:
+        ks = KeySetAll()
+        assert ks.elements() == set()
---
+++ tests/test_some.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+import __future__  # noqa: F401
+
+import key_set  # noqa: F401
+from key_set.base import KeySetSome
+
+
+class TestSome:  # noqa: D101
+
+    def test_represents(self) -> None:
+        ks = KeySetSome({'a', 'b'})
+        assert ks.represents_some()
+        assert not ks.represents_none()
+        assert not ks.represents_all()
+        assert not ks.represents_all_except_some()
+
+    def test_invert(self) -> None:
+        ks = KeySetSome({'a', 'b'})
+        actual = ks.invert()
+        assert actual.represents_all_except_some()
+        assert actual.elements() == {'a', 'b'}
+
+    def test_elements(self) -> None:
+        ks = KeySetSome({'a', 'b'})
+        assert ks.elements() == {'a', 'b'}
