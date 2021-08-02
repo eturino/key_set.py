1bebb4df8d6bd7f519c46abfb73cef60fb0ce4a4
Eduardo Turiño <eturino@eturino.com>
2021-08-02 14:24:17 +0200

    feat: __contains__, __str__, and __repr__

--- key_set/__init__.py
+++ key_set/__init__.py
@@ -5,8 +5,13 @@ The __init__.py files are required to make Python treat directories
 containing the file as packages.
 """
 
-from .base import (KeySet, KeySetAll, KeySetAllExceptSome,  # noqa: F401
-                   KeySetNone, KeySetSome, build_all,
-                   build_all_except_some_or_all, build_none,
-                   build_some_or_none)
+from .base import build_some_or_none  # noqa: F401
+from .base import (KeySet, KeySetAll, KeySetAllExceptSome, KeySetNone,
+                   KeySetSome, build_all, build_all_except_some_or_all,
+                   build_none)
 from .enum import KeySetType  # noqa: F401
+
+__all__ = ['KeySetType', 'KeySet', 'KeySetAll', 'KeySetAllExceptSome',
+           'KeySetNone', 'KeySetSome', 'build_all',
+           'build_all_except_some_or_all', 'build_none',
+           'build_some_or_none']  # noqa: F401
